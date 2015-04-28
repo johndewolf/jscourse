c9388aefc7fac86474cd2274b036146e35ffde61
John DeWolf <jdewolf@backupify.com>
2015-04-28 17:25:19 -0400

    slight update

--- week2/assignment1.js
+++ week2/assignment1.js
@@ -1,6 +1,6 @@
-var evenlyDivisible = function(num) {
+var evenlyDivisible = function(num, max) {
   var counter = 0;
-  for (var x=1; x <= 20; x++) {
+  for (var x=1; x <= max; x++) {
     if (num % x > 0) {
     counter += 1;
     break;
@@ -14,7 +14,7 @@ var evenlyDivisible = function(num) {
 };
 
 numTracker = 40;
-while (evenlyDivisible(numTracker) === false) {
+while (evenlyDivisible(numTracker, 20) === false) {
   numTracker += 1;
 }
 
