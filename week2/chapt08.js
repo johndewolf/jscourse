6bffdfc9351b1b36358a33745bf25539283299b5
John DeWolf <jdewolf@backupify.com>
2015-05-14 17:17:32 -0400

    chapt 8 exercise 1

---
+++ week2/chapt08.js
@@ -0,0 +1,29 @@
+
+// Say you have a function primitiveMultiply that, in 50 percent of cases, multiplies two numbers, and in the other 50 percent, raises an exception of type MultiplicatorUnitFailure. Write a function that wraps this clunky function and just keeps trying until a call succeeds, after which it returns the result.
+
+// Make sure you handle only the exceptions you are trying to handle.
+
+function MultiplicatorUnitFailure() {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.5)
+    return a * b;
+  else
+    throw new MultiplicatorUnitFailure();
+}
+
+function reliableMultiply(a, b) {
+  for (;;) {
+    try {
+      var num = primitiveMultiply(a,b);
+      console.log(num);
+      break;
+    } catch (e) {
+      if (e instanceof MultiplicatorUnitFailure) {
+        console.log('bad number');
+      } else {
+        throw e;
+      }
+    }
+  }
+}
